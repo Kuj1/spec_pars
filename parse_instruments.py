92fa71b477a97330b6d2503366cd6b0a6dd51fd2
Kuj1 <the.same.dentist@gmail.com>
2022-12-07 21:52:47 +0300

    refactored code

--- parse_instruments.py
+++ parse_instruments.py
@@ -58,7 +58,14 @@ def get_data():
             soup = BeautifulSoup(driver.page_source, 'html.parser')
 
             search_page = soup.find('div', class_='search-page').find('div', attrs={'data-behavior': "product-listing"})
-            item = search_page.find('div', attrs={'data-position': '1'}).find('div', class_='title')
+
+            try:
+                item = search_page.find('div', attrs={'data-position': '1'}).find('div', class_='title')
+            except Exception:
+                item = search_page.\
+                    find('div', attrs={'data-position': '1', 'class': 'product-tile grid-item -not-available'}).\
+                    find('div', class_='title')
+
             item_url = item.find('a').get('href')
             if item_url.startswith('https'):
                 driver.get(item_url)
@@ -72,24 +79,99 @@ def get_data():
 
             soup_image = BeautifulSoup(driver.page_source, 'html.parser')
 
-            spec_list = soup_image.find('section', class_='product-description').\
-                find('div', class_='main').find('div', class_='features spoiler').\
-                find('ul', class_='dotted-list').find_all('li', class_='item')
+            mid_specs_array = dict()
 
-            specs_array = dict()
-            for spec in spec_list:
-                spec_name = spec.find('div', class_='option').find('div', class_='title').find('span', class_='text').text.strip()
+            try:
+                spec_list = soup_image.find('section', class_='product-description').\
+                    find('div', class_='main').find('div', class_='features spoiler').\
+                    find('ul', class_='dotted-list').find_all('li', class_='item')
+                for spec in spec_list:
+                    spec_name = spec.find('div', class_='option').find('div', class_='title').find('span', class_='text').text.strip()
 
-                try:
-                    spec_value = spec.find('span', class_='value').text.strip()
-                except Exception:
-                    spec_value = spec.find('div', class_='value').text.strip()
+                    try:
+                        spec_value = spec.find('span', class_='value').text.strip()
+                    except Exception:
+                        spec_value = spec.find('div', class_='value').text.strip()
 
-                specs_array[f'{spec_name}'] = f'{spec_value}'
+                    mid_specs_array[f'{spec_name}'] = f'{spec_value}'
+            except Exception:
+                mid_specs_array['Характеристик'] = 'Нет'
+
+            mid_advantages_array = list()
+
+            try:
+                advantages_item = soup_image.find('div', class_='advantages spoiler').\
+                    find('div', class_='content-block').find('ul').find_all('li')
+
+                for i in advantages_item:
+                    advantage = i.text.strip()
+                    mid_advantages_array.append(advantage)
+            except Exception:
+                mid_advantages_array.append('Нет преимуществ')
+
+            mid_description_array = list()
+
+            try:
+                description_item = soup_image.find('div', attrs={'itemprop': 'description'}).find_all('p')
+
+                for i in description_item:
+                    description = i.text.replace('&nbsp;', '').strip()
+                    mid_description_array.append(description)
+            except Exception:
+                mid_description_array.append('Нет описания')
+
+            try:
+                brand_item = soup_image.find('div', class_='brand').find('img').get('alt')
+            except Exception:
+                brand_item = 'Нет брэнда'
+
+            try:
+                homeland = soup_image.find_all('ul', class_='unordered-list')[0].find_all('li')[0].\
+                    find('span').text.split('—')[0].strip()
+            except Exception:
+                homeland = 'Нет родины'
+
+            try:
+                manufacturer = soup_image.find_all('ul', class_='unordered-list')[0].find_all('li')[1].\
+                    find('span').text.split('—')[0].strip()
+            except Exception:
+                manufacturer = 'Нет производителя'
+
+            mid_equipment_array = list()
+
+            try:
+                equipments_item = soup_image.find('div', class_='equipment spoiler').\
+                    find('div', attrs={'data-selector': 'product-equipment'}).find('ul').find_all('li')
+
+                for i in equipments_item:
+                    equipment = i.text.strip()
+                    mid_equipment_array.append(equipment)
+            except Exception:
+                mid_equipment_array.append('Нет комплектации')
+
+            mid_wrapper_array = list()
+
+            try:
+                wrappers_item = soup_image.find_all('ul', class_='unordered-list')[1].find_all('li')
+
+                for i in wrappers_item:
+                    wrapper_desc = i.text.strip()
+                    mid_wrapper_array.append(wrapper_desc)
+            except Exception:
+                mid_wrapper_array.append('Нет информации об упаковке')
 
             result_specs.append(
                 {
-                    f'{article}': specs_array
+                    f'{article}': {
+                        'Brand': brand_item,
+                        'Homeland': homeland,
+                        'Manufacturer': manufacturer,
+                        'Equipment': mid_equipment_array,
+                        'WrapperInformation': mid_wrapper_array,
+                        'Description': mid_description_array,
+                        'Specifications': mid_specs_array,
+                        'Advantages': mid_advantages_array,
+                    }
                 }
             )
 
