13b7ec369799cb62b621c7a1fef3b86d0d10739b
Kuj1 <the.same.dentist@gmail.com>
2022-12-07 09:00:15 +0300

    refactored code

--- parse_instruments.py
+++ parse_instruments.py
@@ -1,57 +1,112 @@
+import os
+import threading
+import time
+import re
+import json
+from datetime import datetime
+
 import undetected_chromedriver as uc
 from bs4 import BeautifulSoup
+from selenium.webdriver.chrome.service import Service
 
 UA = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) '\
                  'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'
 
-URL = 'https://www.vseinstrumenti.ru/search_main.php?what=DF333DWYE'
+data = os.path.join(os.getcwd(), 'vse.instr')
+user_data = os.path.join(os.getcwd(), 'user_data')
+drivers_dict = dict()
 
-options = uc.ChromeOptions()
-options.headless = True
-options.keep_alive = True
+if not os.path.exists(data):
+    os.mkdir(data)
 
 
-def get_data(url):
-    driver = uc.Chrome(options=options)
+# options = uc.ChromeOptions()
+# options.headless = True
+# options.keep_alive = True
 
-    try:
-        driver.get(url)
 
-        soup = BeautifulSoup(driver.page_source, 'html.parser')
+def get_articles():
+    articles = list()
+    with open(os.path.join(user_data, 'vse.instr_article.txt'), 'r') as doc:
+        for article in doc.readlines():
+            mod_article = article.replace('\n', '').strip()
+            articles.append(mod_article)
 
-        search_page = soup.find('div', class_='search-page').find('div', attrs={'data-behavior': "product-listing"})
-        item = search_page.find_all('div', class_='product-tile grid-item')[0].find('div', class_='title')
-        item_url = item.find('a').get('href')
-        print(item_url)
+    return articles
 
-        driver.get(item_url)
 
-        soup_image = BeautifulSoup(driver.page_source, 'html.parser')
+def get_data():
+    articles = get_articles()
+    date_time = datetime.now().strftime('%d.%m.%Y_%H:%M')
 
-        spec_list = soup_image.find('section', class_='product-description').\
-            find('div', class_='main').find('div', class_='features spoiler').\
-            find('ul', class_='dotted-list').find_all('li', class_='item')
+    result_specs = list()
+    # s = Service(f'{os.getcwd()}/chromedriver')
+    for article in articles:
+        print(f'[+] Article: {article}')
+        url = 'https://www.vseinstrumenti.ru/search_main.php?what='
+        mod_url = f'{url}{article}'
 
-        for spec in spec_list:
-            spec_name = spec.find('div', class_='option').find('div', class_='title').find('span', class_='text').text.strip()
+        options = uc.ChromeOptions()
+        options.headless = True
+        options.keep_alive = True
 
-            try:
-                spec_value = spec.find('span', class_='value').text.strip()
-            except Exception as ex:
-                spec_value = spec.find('div', class_='value').text.strip()
+        driver = uc.Chrome(options=options)
 
-            print(f'{spec_name}: {spec_value}')
+        try:
+            driver.get(mod_url)
 
-    except Exception as ex:
-        print(ex)
+            soup = BeautifulSoup(driver.page_source, 'html.parser')
 
-    finally:
-        driver.close()
-        driver.quit()
+            search_page = soup.find('div', class_='search-page').find('div', attrs={'data-behavior': "product-listing"})
+            item = search_page.find('div', attrs={'data-position': '1'}).find('div', class_='title')
+            item_url = item.find('a').get('href')
+            if item_url.startswith('https'):
+                driver.get(item_url)
+                time.sleep(2)
+                print(f'\t[+] Go to the item page: {item_url}')
+            else:
+                mod_item_url = f'https://www.vseinstrumenti.ru{item_url}'
+                driver.get(mod_item_url)
+                time.sleep(2)
+                print(f'\t[+] Go to the item page: {mod_item_url}')
+
+            soup_image = BeautifulSoup(driver.page_source, 'html.parser')
+
+            spec_list = soup_image.find('section', class_='product-description').\
+                find('div', class_='main').find('div', class_='features spoiler').\
+                find('ul', class_='dotted-list').find_all('li', class_='item')
+
+            specs_array = dict()
+            for spec in spec_list:
+                spec_name = spec.find('div', class_='option').find('div', class_='title').find('span', class_='text').text.strip()
+
+                try:
+                    spec_value = spec.find('span', class_='value').text.strip()
+                except Exception:
+                    spec_value = spec.find('div', class_='value').text.strip()
+
+                specs_array[f'{spec_name}'] = f'{spec_value}'
+
+            result_specs.append(
+                {
+                    f'{article}': specs_array
+                }
+            )
+
+        except Exception as ex:
+            driver.save_screenshot('ew.png')
+            print(f'\t[-] {ex}')
+
+        finally:
+            driver.close()
+            driver.quit()
+
+    with open(os.path.join(data, f'vse.instr_{date_time}.json'), 'a') as file:
+        json.dump(result_specs, file, ensure_ascii=False, indent=4)
 
 
 def main():
-    get_data(url=URL)
+    get_data()
 
 
 if __name__ == '__main__':
--- parse_market.py
+++ parse_market.py
@@ -1,11 +1,12 @@
-import os.path
+import os
 import time
 import re
+import json
+from datetime import datetime
 
-import undetected_chromedriver as uc
 import requests
 from bs4 import BeautifulSoup
-from selenium.common import TimeoutException
+from selenium import webdriver
 from selenium.webdriver import Keys
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
@@ -20,26 +21,74 @@ HEADERS = {
                  'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'
 }
 
-URL = 'https://market.yandex.ru/search?text=DF333DWYE'
+API = 'dcd30132e329c316dcd003ebcb12f980'
+
+# URL = 'https://market.yandex.ru/search?text=DF333DWYE'
 
 captcha_img = os.path.join(os.getcwd(), 'captcha_img')
+data = os.path.join(os.getcwd(), 'ya.market')
+user_data = os.path.join(os.getcwd(), 'user_data')
 
 if not os.path.exists(captcha_img):
     os.mkdir(captcha_img)
 
-options = uc.ChromeOptions()
-options.headless = True
+if not os.path.exists(data):
+    os.mkdir(data)
+
+
+options = webdriver.ChromeOptions()
+options.add_argument('--disable-blink-features=AutomationControlled')
+options.add_argument(f'--user-agent={UA}')
+options.add_argument('start-maximized')
+options.add_argument('--headless')
+options.add_argument('--enable-javascript')
+
+
+def bypass_text_captcha(api, img):
+    path_img = os.path.join(captcha_img, img)
+
+    with open(path_img, "rb") as img_file:
+
+        data = (
+            ('key', (None, f'{api}')),
+            ('file', img_file),
+            )
+
+        post_captcha = requests.post(f'http://rucaptcha.com/in.php', files=data, headers=HEADERS)
+        id_captcha = post_captcha.text.strip().split('|')[1]
+        time.sleep(5)
+
+        res_captcha = requests.get(f'http://rucaptcha.com/res.php?key={api}&action=get&id={id_captcha}')
+        result = res_captcha.text
+        if result == 'CAPCHA_NOT_READY':
+            while result == 'CAPCHA_NOT_READY':
+                res_captcha = requests.get(f'http://rucaptcha.com/res.php?key={api}&action=get&id={id_captcha}')
+                result = res_captcha.text
+                time.sleep(5)
+                if result != 'CAPCHA_NOT_READY':
+                    result = res_captcha.text.strip().split('|')[1]
+                    return result
+        else:
+            result = res_captcha.text.strip().split('|')[1]
+            return result
 
 
 def get_photo(url, id_img):
     try:
         img_resp = requests.get(url, headers=HEADERS)
 
-        with open(os.path.join(captcha_img, f'{id_img}.jpg'), 'wb') as file:
+        name_img = f'{id_img}.jpg'
+
+        with open(os.path.join(captcha_img, name_img), 'wb') as file:
             file.write(img_resp.content)
 
     except Exception as ex:
         print(ex)
+
+
+def input_elem(elem, key, key_bind):
+    elem.clear()
+    elem.send_keys(key, key_bind)
 
 
 def bypass_captcha(driver, timeout):
@@ -47,7 +96,7 @@ def bypass_captcha(driver, timeout):
     try:
         WebDriverWait(driver, timeout). \
             until(EC.presence_of_element_located((By.XPATH, '//input[@type="submit"]'))).click()
-        print('[+] Captcha solved')
+        print('\t[+] SmartCaptcha solved')
 
         # Block of code, if after click captcha goes text captcha
         try:
@@ -59,60 +108,110 @@ def bypass_captcha(driver, timeout):
 
                 src_img = soup.find('div', class_='AdvancedCaptcha'). \
                     find('div', class_='AdvancedCaptcha-View').find('img', class_='AdvancedCaptcha-Image').get('src')
-                print(src_img)
+                print(f'[+] Captcha image: {src_img}')
                 img_id = src_img.split('/')[-1]
 
                 get_photo(url=src_img, id_img=img_id)
+                text_captcha_result = bypass_text_captcha(api=API, img=f'{img_id}.jpg')
+
+                enter_captcha = WebDriverWait(driver, timeout).\
+                    until(EC.presence_of_element_located((By.CLASS_NAME, 'Textinput-Control')))
+                input_elem(enter_captcha, text_captcha_result, Keys.ENTER)
+
+                print('\t[+] TextCaptcha solved')
+                print(f'\t\t[+] Answer: {text_captcha_result}')
             else:
                 pass
 
         except Exception as ex:
-            print('[+] No SmartCaptcha')
+            print('\t[+] No TextCaptcha')
 
     except Exception as ex:
-        print('[+] No captcha at all')
+        print('\t[+] No SmartCaptcha')
 
 
-def get_data(url):
-    driver = uc.Chrome(options=options)
-    timeout = 5
-    driver.set_page_load_timeout(timeout)
+def get_articles():
+    articles = list()
+    with open(os.path.join(user_data, 'ya.market_article.txt'), 'r') as doc:
+        for article in doc.readlines():
+            mod_article = article.replace('\n', '').strip()
+            articles.append(mod_article)
 
-    try:
-        driver.get(url)
+    return articles
 
-        bypass_captcha(driver=driver, timeout=timeout)
-        # time.sleep(15)
 
-        items_soup = BeautifulSoup(driver.page_source, 'html.parser')
+def get_data():
+    articles = get_articles()
+    date_time = datetime.now().strftime('%d.%m.%Y_%H:%M')
 
-        item = items_soup.find('main', attrs={'id': 'main'}).find('div', attrs={'data-test-id': 'virtuoso-item-list'}).find_all('div')[1].find('a').get('href').strip().split('?')[0]
-        item_url = f'https://market.yandex.ru{item}'
-        print(f'[+] Go to the item page: {item_url}')
+    result_specs = list()
+    for article in articles:
+        print(f'[+] Article: {article}')
+        url = 'https://market.yandex.ru/search?text='
+        mod_url = f'{url}{article}'
 
-        driver.get(item_url)
-        driver.save_screenshot('fi1.png')
-        # bypass_captcha(driver=driver, timeout=timeout)
+        s = Service(f'{os.getcwd()}/chromedriver')
+        driver = webdriver.Chrome(options=options, service=s)
+        timeout = 5
 
-        spec_soup = BeautifulSoup(driver.page_source, 'html.parser')
-        spec = spec_soup.find('a', string=re.compile('Характеристики')).get('href').strip()
-        spec_url = f'https://market.yandex.ru{spec}'
-        print(f"[+] Go to the item specs page: {spec_url}")
+        try:
+            driver.get(mod_url)
+            bypass_captcha(driver=driver, timeout=timeout)
 
-        driver.get(spec_url)
-        driver.save_screenshot('fi2.png')
+            items_soup = BeautifulSoup(driver.page_source, 'html.parser')
 
-    except TimeoutException as ex:
-        driver.save_screenshot('ew.png')
-        print(f'[-] {ex}')
+            item = items_soup.find('main', attrs={'id': 'main'}).find('div', attrs={'data-test-id': 'virtuoso-item-list'}).find_all('div')[1].find('a').get('href').strip().split('?')[0]
+            item_url = f'https://market.yandex.ru{item}'
+            print(f'\t[+] Go to the item page: {item_url}')
 
-    finally:
-        driver.close()
-        driver.quit()
+            driver.get(item_url)
+            bypass_captcha(driver=driver, timeout=timeout)
+
+            spec_soup = BeautifulSoup(driver.page_source, 'html.parser')
+            spec = spec_soup.find('a', string=re.compile('Характеристики')).get('href').strip()
+            spec_url = f'https://market.yandex.ru{spec}'
+            print(f"\t[+] Go to the item specs page: {spec_url}")
+
+            driver.get(spec_url)
+            bypass_captcha(driver=driver, timeout=timeout)
+            time.sleep(3)
+
+            specs_soup = BeautifulSoup(driver.page_source, 'html.parser')
+
+            specs = specs_soup.find('div', attrs={'data-auto': 'product-full-specs'}).find_all('div', class_='la3zd')
+
+            specs_array = dict()
+            for each_section_spec in specs:
+                section_specs = each_section_spec.find('div', class_='_18fxQ').find_all('dl', class_='sZB0N')
+                for each_spec in section_specs:
+                    name_spec = each_spec.find('dt', class_='_1viar').find('div', class_='_2TxqA').find('span').\
+                        text.strip()
+                    try:
+                        value_spec = each_spec.find('div', class_='cia-vs cia-cs').find('dd').text.strip()
+                    except Exception:
+                        value_spec = each_spec.find('div', class_='_2Yndd').find('dd').text.strip()
+
+                    specs_array[f'{name_spec}'] = f'{value_spec}'
+
+            result_specs.append(
+                {
+                    f'{article}': specs_array
+                }
+            )
+
+        except Exception as ex:
+            print(f'\t[-] {ex}')
+
+        finally:
+            driver.close()
+            driver.quit()
+
+    with open(os.path.join(data, f'ya.market_{date_time}.json'), 'a') as file:
+        json.dump(result_specs, file, ensure_ascii=False, indent=4)
 
 
 def main():
-    get_data(url=URL)
+    get_data()
 
 
 if __name__ == '__main__':
